8784cfd1d8439a593b842e77dbbaf2d4544c5e64
Yadav._51 <151940634+REX-51@users.noreply.github.com>
2024-11-29 07:27:35 +0530

    update

--- src/safe.java
+++ src/safe.java
@@ -1,10 +1,3 @@
-public class safe {
-    public static void main(String[] args) {
-        int a = 34;
-        int b = 78;
-        int sum = a + b;
-//        System.out.println(sum);
-
-//        System.out.println("The sum is "+sum);
-    }
+public class safe{
+    public static vodi 
 }
